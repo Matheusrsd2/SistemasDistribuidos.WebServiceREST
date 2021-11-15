875ee944722c33782e6aa8cc5013c46aa85758bf
unknown <matheusreis@NOTE-MATRSANTOS.gruponp.local>
2021-11-15 14:14:15 -0300

    Projeto concluido

---
+++ WebService.REST.SistemasDIstribuidos/Controllers/AlunoController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.REST.SistemasDIstribuidos.Model;
+
+namespace WebService.REST.SistemasDIstribuidos.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private readonly Aluno _aluno;
+        public AlunoController(Aluno aluno)
+        {
+            _aluno = aluno;
+        }
+
+        /* Obtém todos os alunos */
+        [Route("api/alunos/v1")]
+        [HttpGet]
+        public ActionResult ObterAlunos()
+        {
+            List<Aluno> alunos = _aluno.ListarAlunos();
+            return Ok(alunos);
+        }
+
+        /* Obtém o aluno espefífico pelo nome */
+        [Route("api/alunos/v1/{nome}")]
+        [HttpGet]
+        public ActionResult ObterAlunoPeloNome(string nome)
+        {
+            Aluno aluno = _aluno.ObterAlunoPeloNome(nome);
+            if(aluno != null)
+            {
+                return Ok(aluno);
+            }
+            return StatusCode(404, "Aluno não foi localizado");
+
+        }
+
+        /* Adiciona um aluno novo na lista */
+        [Route("api/alunos/v1")]
+        [HttpPost]
+        public ActionResult IncluirAluno ([FromBody] Aluno aluno)
+        {
+            try
+            {
+                _aluno.IncluirAluno(aluno);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            return Created($"api/alunos/v1/{aluno}", aluno);
+            
+        }
+
+        /* Exclui o aluno da lista através do nome */
+        [Route("api/alunos/v1/{nome}")]
+        [HttpDelete]
+        public ActionResult RemoverAluno (string nome)
+        {
+            _aluno.RemoverAluno(nome);
+            return Ok();
+        }
+
+        /*Edita o aluno buscando pelo nome */
+        [Route("api/alunos/v1/{nome}")]
+        [HttpPut]
+        public ActionResult EditarAluno(string nome, [FromBody] Aluno model)
+        {
+            _aluno.EditarAluno(nome, model);
+            return Ok();
+        }
+    }
+}
---
+++ WebService.REST.SistemasDIstribuidos/Model/Aluno.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.REST.SistemasDIstribuidos.Model
+{
+    public class Aluno
+    {
+        public string NomeAluno { get; set; }
+        public string Curso { get; set; }
+        public string Semestre { get; set; }
+        public int RA { get; set; }
+        public string CPF { get; set; }
+        public string Cidade { get; set; }
+
+        static List<Aluno> alunos = new List<Aluno>();
+
+        public List<Aluno> ListarAlunos()
+        {
+            return alunos;
+        }
+
+        public Aluno ObterAlunoPeloNome(string nome)
+        {
+            Aluno aluno = alunos.Find(x => x.NomeAluno == nome);
+            return aluno;
+        }
+
+        public void IncluirAluno(Aluno aluno)
+        {
+            alunos.Add(aluno);
+        }
+
+        public void RemoverAluno(string nome)
+        {
+            Aluno aluno = alunos.Find(x => x.NomeAluno == nome);
+            alunos.Remove(aluno);
+        }
+
+        public void EditarAluno(string nome, Aluno model)
+        {
+            Aluno aluno = alunos.FirstOrDefault(a => a.NomeAluno == nome);
+            if (aluno != null)
+            {
+                aluno.NomeAluno = model.NomeAluno;
+                aluno.Curso = model.Curso;
+                aluno.Semestre = model.Semestre;
+                aluno.RA = model.RA;
+                aluno.CPF = model.CPF;
+                aluno.Cidade = model.Cidade;
+            }
+        }       
+    }
+}
